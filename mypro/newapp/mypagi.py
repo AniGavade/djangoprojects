4fbede999adf6ce0734c4af1cf84de61db10d45d
Aniruddha <aniruddhag87@live.com>
2022-11-02 19:07:38 +0530

    'new commit by Aniruddha'

--- mypro/newapp/mypagi.py
+++ mypro/newapp/mypagi.py
@@ -1,3 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+
+
 class MyPagination(PageNumberPagination):
-    page_size=2
+    page_size = 2
--- mypro/newapp/serializers.py
+++ mypro/newapp/serializers.py
@@ -1,32 +1,39 @@
 from rest_framework import serializers
 from .models import Mobile, users
 
+
 class userserializer(serializers.Serializer):
-    name=serializers.CharField(max_length=120)
-    email=serializers.CharField(max_length=120)
-    phone=serializers.IntegerField()
-    password=serializers.CharField(max_length=120)
+    name = serializers.CharField(max_length=120)
+    email = serializers.CharField(max_length=120)
+    phone = serializers.IntegerField()
+    password = serializers.CharField(max_length=120)
+
     def create(self, validated_data):
         return users.objects.create(**validated_data)
-    def update(self,instance,validated_data):
+
+    def update(self, instance, validated_data):
         print(instance.name)
-        instance.name=validated_data.get('name',instance.name)
-        instance.email=validated_data.get("email",instance.email)
-        instance.phone=validated_data.get("phone",instance.phone)
-        instance.password=validated_data.get('password',instance.password)
+        instance.name = validated_data.get('name',instance.name)
+        instance.email = validated_data.get("email",instance.email)
+        instance.phone = validated_data.get("phone",instance.phone)
+        instance.password = validated_data.get('password',instance.password)
         instance.save()
         return instance
-    def validate_phone(self,value):
+
+    def validate_phone(self, value):
         if value>1000:
             raise serializers.ValidationError("please enter valid number")
         return value
+
+
 class modelserializer(serializers.ModelSerializer):
     class Meta:
         model = users 
-        fields="__all__"
+        fields = "__all__"
+
 
 class mobileserializer(serializers.ModelSerializer):
     class Meta:
         model = Mobile 
-        fields="__all__"
-
+        fields = "__all__"
+        
